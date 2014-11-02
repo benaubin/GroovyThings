b1f751ce05a73cd18a736247e9208aeee08d8ca3
Ben! <penne12@allforfunmc.com>
2014-11-02 14:16:55 -0600

    A little bit of cleanup

--- src/com/bensites/java/groovy/things/Main.groovy
+++ src/com/bensites/java/groovy/things/Main.groovy
@@ -3,14 +3,9 @@ package com.bensites.java.groovy.things
 class Main {
 
 	static main(args) {
-		Person Ben = new Person("Ben", "Aubin", "Male")
-		Person Mitch = new Person("Mitch", "Aubin", "Male")
-		Ben.relateTo(Mitch, Relation.Son, false)
-		Mitch.relateTo(Ben, Relation.Dad, false)
-		println(Ben)
-		println(Mitch)
-		println("$Ben is related to $Mitch as " + Ben.pronoun() + " " + Ben.relatedAs(Mitch).toString())
-		println("$Mitch is related to $Ben as " + Mitch.pronoun() + " " + Mitch.relatedAs(Ben).toString()) 
+		def ListObject<int> smallest = new ListObject(1,1);
+		smallest.setObject(null)
+		print(smallest.getObject().getMetaClass().toString())
 	}
 
 }
