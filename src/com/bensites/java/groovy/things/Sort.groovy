2be33c0baad502fb93f0c6709ed8463a1d5fee32
Ben! <penne12@allforfunmc.com>
2014-11-02 14:17:04 -0600

    Sorting is coming

---
+++ src/com/bensites/java/groovy/things/Sort.groovy
@@ -0,0 +1,16 @@
+package com.bensites.java.groovy.things
+
+class Sort {
+	
+	def static Double[] Scan(Double[] toSort) {
+		boolean sorting = true;
+		double smallest;
+		double smallestindex;
+		while(sorting) {
+			for(double i in toSort) {
+				
+			}
+		}
+	}
+
+}
