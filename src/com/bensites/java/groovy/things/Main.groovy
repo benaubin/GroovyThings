553fc0f015dfb626427267adfc0c963c20cee5d8
Ben! <penne12@allforfunmc.com>
2014-11-04 07:43:10 -0600

    Not much

--- src/com/bensites/java/groovy/things/Main.groovy
+++ src/com/bensites/java/groovy/things/Main.groovy
@@ -3,9 +3,7 @@ package com.bensites.java.groovy.things
 class Main {
 
 	static main(args) {
-		def ListObject<int> smallest = new ListObject(1,1);
-		smallest.setObject(null)
-		print(smallest.getObject().getMetaClass().toString())
+		
 	}
 
 }
